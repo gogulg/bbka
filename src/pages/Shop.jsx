b5132334cda1387a44ffeb17386b4a0dadbf8e50
Gogul <gokulgalaxy47@gmail.com>
2026-03-07 04:10:32 +0530

    Changed View

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,74 +1,76 @@
 import React from "react";
 import logo4 from "../assets/cor.png";
-import on from "../assets/on.jpg"
-import on1 from "../assets/on1.jpg"
-import on2 from "../assets/on2.jpg"
-import on3 from "../assets/on3.jpg"
-import on4 from "../assets/on4.jpg"
-
+import on from "../assets/on.jpg";
+import on1 from "../assets/on1.jpg";
+import on2 from "../assets/on2.jpg";
+import on3 from "../assets/on3.jpg";
+import on4 from "../assets/on4.jpg";
 
 const Shop = () => {
-  return <div class="grid grid-cols-4 col-end-2  gap-3 m-5">
-  <img
-          src={on1}
-          alt="Company Logo"
-          className="w-screen object-cover  rounded-2xl shadow-xl"
-        />
-              <img
-          src={on4}
-          alt="Company Logo"
-          className=" w-screen object-cover  rounded-2xl shadow-xl"
-        />
-         <img
-          src={on3}
-          alt="Company Logo"
-          className="w-screen object-cover  rounded-2xl shadow-xl"
-        />
-              <img
-          src={on2}
-          alt="Company Logo"
-          className=" w-screen object-cover  rounded-2xl shadow-xl"
-        />
-         <img
-          src={on1}
-          alt="Company Logo"
-          className="w-screen object-cover  rounded-2xl shadow-xl"
-        />
-              <img
-          src={on}
-          alt="Company Logo"
-          className=" w-screen object-cover  rounded-2xl shadow-xl"
-        />
-          <img
-          src={on1}
-          alt="Company Logo"
-          className="w-screen object-cover  rounded-2xl shadow-xl"
-        />
-              <img
-          src={on4}
-          alt="Company Logo"
-          className=" w-screen object-cover  rounded-2xl shadow-xl"
-        />
-         <img
-          src={on3}
-          alt="Company Logo"
-          className="w-screen object-cover  rounded-2xl shadow-xl"
-        />
-              <img
-          src={on2}
-          alt="Company Logo"
-          className=" w-screen object-cover  rounded-2xl shadow-xl"
-        />
-         <img
-          src={on1}
-          alt="Company Logo"
-          className="w-screen object-cover  rounded-2xl shadow-xl"
-        />
-              <img
-          src={on}
-          alt="Company Logo"
-          className=" w-screen object-cover  rounded-2xl shadow-xl"
-        /></div>;
+  return (
+   <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 m-6">
+      <img
+        src={on1}
+        alt="Company Logo"
+        className="w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on4}
+        alt="Company Logo"
+        className=" w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on3}
+        alt="Company Logo"
+        className="w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on2}
+        alt="Company Logo"
+        className=" w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on1}
+        alt="Company Logo"
+        className="w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on}
+        alt="Company Logo"
+        className=" w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on1}
+        alt="Company Logo"
+        className="w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on4}
+        alt="Company Logo"
+        className=" w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on3}
+        alt="Company Logo"
+        className="w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on2}
+        alt="Company Logo"
+        className=" w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on1}
+        alt="Company Logo"
+        className="w-screen object-cover  rounded-2xl shadow-xl"
+      />
+      <img
+        src={on}
+        alt="Company Logo"
+        className=" w-screen object-cover  rounded-2xl shadow-xl"
+      />
+    </div>
+  );
 };
 
 export default Shop;
